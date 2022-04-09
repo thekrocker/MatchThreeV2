9ab0dd3d02bdab38064a88c8edcc5f9e2d676bf5
ging_safa <sdedeakay@yahoo.com>
2022-04-09 21:36:57 +0300

    feat: Add feel package

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -8,32 +8,16 @@ namespace MatchThree
 {
     public class GamePiece : MonoBehaviour
     {
-        public int _xIndex;
-        public int _yIndex;
+        public int xIndex;
+        public int yIndex;
 
 
         public void SetCoordinates(int x, int y)
         {
-            _xIndex = x;
-            _yIndex = y;
+            xIndex = x;
+            yIndex = y;
         }
-
-
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                Move(((int)transform.position.x + 1), (int)transform.position.y);
-
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                Move(((int)transform.position.x - 1), (int)transform.position.y);
-            }
-
-            
-        }
+        
 
         private bool _isMoving;
         private void Move(int destinationX, int destinationY)
