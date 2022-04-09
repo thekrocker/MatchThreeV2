36cb7a6041915a8e21ab04b5b4caa7c3814b293e
ging_safa <sdedeakay@yahoo.com>
2022-04-09 21:21:29 +0300

    feat: Import essential assets

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace MatchThree
@@ -14,6 +16,32 @@ namespace MatchThree
         {
             _xIndex = x;
             _yIndex = y;
+        }
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Move(((int)transform.position.x + 1), (int)transform.position.y);
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Move(((int)transform.position.x - 1), (int)transform.position.y);
+            }
+
+            
+        }
+
+        private bool _isMoving;
+        private void Move(int destinationX, int destinationY)
+        {
+            if (_isMoving) return;
+            _isMoving = true;
+            transform.DOMove(new Vector3(destinationX, destinationY, 0), 0.5f).SetEase(Ease.Linear).OnComplete(() => _isMoving = false);
+            
         }
     }
 }
